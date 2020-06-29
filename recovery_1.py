629d8b118f43c7877d8da1086861741fdef1a171
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2020-06-29 18:22:10 +0600

    recovery

---
+++ recovery_1.py
@@ -0,0 +1,8 @@
+# # find the sum between 1 to n using formula
+# n=int(input("last number:"))
+# sum=(n*(n+1))/2
+# print(f"sum = {sum}")
+
+while True:
+    x=input("What is your name?")
+    print(f"hello,{x}")
---
+++ recovery_2.py
@@ -0,0 +1,7 @@
+x_1=float(input("Enter X_1:"))
+x_2=float(input("Enter X_2:"))
+y_1=float(input("Enter Y_1:"))
+y_2=float(input("Enter Y_2:"))
+
+dis=pow(((x_2-x_1)**2+(y_2-y_1)**2),0.5)
+print(f"Distance={dis}")
