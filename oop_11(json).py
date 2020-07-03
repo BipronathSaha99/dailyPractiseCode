6c1438c5524d22360dc621a04d76aa3a43488608
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2020-07-03 13:07:42 +0600

    json

---
+++ oop_11(json).py
@@ -0,0 +1,51 @@
+# Python JSON
+'''JSON is a syntax for storing and exchanging data.
+
+JSON is text, written with JavaScript object notation.
+'''
+#  Convert from JSON to Python
+# import json
+# # some JSON:
+# x =  '{ "name":"John", "age":30, "city":"New York"}'
+# # parse x:
+# # y = json.loads(x)
+# # the result is a Python dictionary:
+# # print(y["age"])
+# print(json.loads(x))
+
+# convert from python to json 
+# import json 
+# x={'name':'Bipro','age':22,'city':'Dhaka'}
+# print(json.dumps(x))
+# x = {
+#   "name": "John",
+#   "age": 30,
+#   "city": "New York"
+# }
+
+# # convert into JSON:
+# y = json.dumps(x)
+
+# # the result is a JSON string:
+# print(y)
+
+# import json
+
+# print(json.dumps({"name": "John", "age": 30}))
+# print(json.dumps(["apple", "bananas"]))
+# print(json.dumps(("apple", "bananas")))
+# print(json.dumps("hello"))
+# print(json.dumps(42))
+# print(json.dumps(31.76))
+# print(json.dumps(True))
+# print(json.dumps(False))
+# print(json.dumps(None))
+# import json 
+# print(json.dumps({'name':'Bipro','city':'Dhaka'}))
+# print(json.dumps(['apple','20']))
+# print(json.dumps(('apple','20')))
+# print(json.dumps("bipro"))
+# print(json.dumps(23))
+# print(json.dumps(True))
+# print(json.dumps(False))
+# print(json.dumps(None))
