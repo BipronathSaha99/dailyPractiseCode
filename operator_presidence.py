2ce8e6fe9424b50f5d61bd3742c5a4a3df008b80
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2020-07-03 13:20:26 +0600

    Add files via upload

---
+++ operator_presidence.py
@@ -0,0 +1,19 @@
+# operator presidency
+# Precedence of or & and
+# meal = "fruit"
+
+# money = 0
+
+# if meal == "fruit" or meal == "sandwich" and money >= 2:
+#     print("Lunch being delivered")
+# else:
+#     print("Can't deliver lunch")
+
+meal = "fruit"
+
+money = 0
+
+if (meal == "fruit" or meal == "sandwich") and money >= 2:
+    print("Lunch being delivered")
+else:
+    print("Can't deliver lunch")
