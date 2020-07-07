e6dec2fe01c0ab9f6b5054cf9f79d48bbec4378b
Bipronath Saha <66156651+BipronathSaha99@users.noreply.github.com>
2020-07-08 00:02:58 +0600

    filter

---
+++ filter.py
@@ -0,0 +1,31 @@
+# filter() in python
+# The filter() method filters the given sequence with 
+# the help of a function that tests each element in the sequence to be true or not.
+
+# function that filters vowel 
+# def myfunc(variable):
+#     letters=['a','e','i','o','u']
+#     if variable in letters:
+#         return True
+#     else:
+#         return False
+
+# #sequance
+# seq=['g','e','a','f','l']
+# #using filter method
+# filtered=filter(myfunc,seq)
+# print("The filered letters are:")
+# for s in filtered:
+#     print(s)
+
+# a list contains both even and odd 
+
+# seq=[0,1,2,3,4,5,6,7,8]
+# #using filter method   
+# res=filter(lambda a:a%2==0,seq)
+# print(list(res))
+
+# res=filter(lambda a:a%2!=0,seq)
+# print(list(res))
+
+
